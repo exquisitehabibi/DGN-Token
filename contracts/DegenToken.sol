adde32a3054666d3fbeec8a5d868792ff19858c0
Arun Thakur <120034666+exquisitehabibi@users.noreply.github.com>
2024-06-13 15:45:04 +0530

    Update DegenToken.sol

--- contracts/DegenToken.sol
+++ contracts/DegenToken.sol
@@ -1,14 +1,17 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
-
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-
 //Minting new tokens: The platform should be able to create new tokens and distribute them to players as rewards. Only the owner can mint tokens.
 //Transferring tokens: Players should be able to transfer their tokens to others.
 //Redeeming tokens: Players should be able to redeem their tokens for items in the in-game store.
 //Checking token balance: Players should be able to check their token balance at any time.
 //Burning tokens: Anyone should be able to burn tokens, that they own, that are no longer needed.
+
+// inherited functions like approve, inc/dec allowance & tranferfrom are not overrided so they accept tokens in wei
+// all required functions accept values in eth (1 * 10 ** decimals() wei) for convenience
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract DegenToken is ERC20, Ownable {
 
